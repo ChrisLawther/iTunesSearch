37d500955c8cdcd4096cab8e93cdd3457a1d95f7
Chris <chris@intriguingdevelopments.com>
2022-02-07 13:31:33 +0000

    .previewUrl is optional on Tracks

--- Sources/iTunesSearch/Response types/Track.swift
+++ Sources/iTunesSearch/Response types/Track.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public struct Track: Decodable {
     public let kind: String    // enum?
-    public let previewUrl: URL
+    public let previewUrl: URL?
     public let trackName: String
 }
